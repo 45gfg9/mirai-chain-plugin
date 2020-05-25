62264d0e823da545d72994b4eb52f49436bd9580
45gfg9 <mc45@qq.com>
2020-05-26 00:19:51 +0800

    First let's put a big hole here

--- src/main/java/net/im45/bot/chain/Chain.java
+++ src/main/java/net/im45/bot/chain/Chain.java
@@ -1,6 +1,26 @@
 package net.im45.bot.chain;
 
+import net.mamoe.mirai.console.command.BlockingCommand;
+import net.mamoe.mirai.console.command.CommandSender;
+import net.mamoe.mirai.console.command.JCommandManager;
 import net.mamoe.mirai.console.plugins.PluginBase;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class Chain extends PluginBase {
+    @Override
+    public void onLoad() {
+        JCommandManager.getInstance().register(this, new BlockingCommand(
+                "chain",
+                List.of(),
+                "Chain!",
+                "chain"
+        ) {
+            @Override
+            public boolean onCommandBlocking(@NotNull CommandSender commandSender, @NotNull List<String> list) {
+                return false;
+            }
+        });
+    }
 }
